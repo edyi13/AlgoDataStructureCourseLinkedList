f326b5383ee8084fc0956b96922fec1a05a987b5
Edson <44821520+edyi13@users.noreply.github.com>
2022-07-26 15:16:23 +0000

    added custom double LinkedList

---
+++ CustomDoubleLinkedList.cs
@@ -0,0 +1,104 @@
+using System;
+
+public class CustomLinkedList
+    {
+        public Node head;
+        public Node tail;        
+        public Node prev;
+        public int length;
+
+        public CustomDoubleLinkedList(object value)
+        {
+            head = new Node(value);
+
+            tail = head;
+            length = 1;
+        }
+
+        public void Append(object value)
+        {
+            var newNode = new Node(value);
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+            length++;
+        }
+
+        public void Preppend(object value)
+        {
+            var newNode = new Node(value);
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+            length++;
+        }
+
+        public object[] GetList()
+        {
+            var arr = new object[length];
+            var currNode = head;
+            var count = 0;
+            while (currNode != null)
+            {
+                if (count == length)
+                    break;
+                arr[count] = currNode.value;
+                currNode = (Node)currNode.next;
+                count++;
+            }
+            return arr;
+        }
+
+        public object[] Insert(int index, object value)
+        {
+            if (index == 0)
+            {
+                Preppend(value);
+                return GetList();
+            }
+            var newNode = new Node(value);
+            Node leaderNode = TraverseToIndex(index-1);
+            Node follower = (Node)leaderNode.next;
+            leaderNode.next = newNode;
+            newNode.prev = leaderNode;
+            newNode.next = follower;
+            follower.prev = newNode;
+            length++;
+            return GetList();
+        }
+
+        public object[] Remove(int index)
+        {
+            if (index > length)
+                return GetList();
+            if (index == 0)
+            {
+                Node currHead = head;
+                head = (Node)currHead.next;
+                length--;
+                if (length == 0)
+                    tail = head;
+
+                return GetList();
+            }
+            Node prevNode = TraverseToIndex(index-1);
+            Node nextNode = (Node)prevNode.next;
+            nextNode.prev = prevNode;
+            prevNode.next = nextNode.next;
+            length--;
+            return GetList();
+        }
+
+        public Node TraverseToIndex(int index)
+        {
+            int counter = 0;
+            var currNode = head;
+            while (counter != index)
+            {
+                currNode = (Node?)currNode.next;
+                counter++;
+            }
+
+            return currNode;
+        }
+    }
